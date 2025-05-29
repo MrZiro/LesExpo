d18211bbe419eaade510a214f9e2b3de53257e60
mrZir00 <adobe.mrziro@gmail.com>
2025-05-29 17:11:03 +0300

    1.2

--- LesExpo.web/Controllers/HomeController.cs
+++ LesExpo.web/Controllers/HomeController.cs
@@ -1,20 +1,43 @@
 using System.Diagnostics;
+using LesExpo.DataAccess.Repository.IRepository;
 using LesExpo.Models;
+using LesExpo.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 namespace LesExpo.web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var sliders = _unitOfWork.Slider.GetAll()
+                    .Where(s => s.IsActive)
+                    .OrderBy(s => s.DisplayOrder)
+                    .ToList();
+
+        // Get latest published blogs for the blog slider
+        var blogs = _unitOfWork.Blog.GetAll()
+                    .Where(b => b.IsPublished)
+                    .OrderByDescending(b => b.CreatedAt)
+                    .Take(3) // Match the 3 slides in the original design
+                    .ToList();
+
+        // Create and populate ViewModel
+        var homeVM = new HomeViewModel
+        {
+            Sliders = sliders,
+            Blogs = blogs
+        };
+
+        return View(homeVM);
     }
 
     public IActionResult Privacy()
